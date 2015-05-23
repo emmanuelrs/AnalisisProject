919f5c3fa46e4a1eed6f604aa0a9cce121841444
Emmanuel Rosales <emmanuelrs@gmail.com>
2015-05-23 11:33:48 -0600

    cambia de pantalla

--- PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/Trucks.java
+++ PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/Trucks.java
@@ -4,22 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Trucks extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trucks);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_trucks, menu);
         return true;
     }
 
@@ -36,10 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void jumpInsertT(View view) {
-        Intent intent = new Intent(view.getContext(), Trucks.class);
-        startActivityForResult(intent, 0);
     }
 }
