df4b5689e583c2af11591fc1d8bcfefc058691d6
miumiu997 <miuyinyong@yahoo.com>
2015-05-30 15:37:48 -0600

    subspace

---
+++ Logica/src/Logic/main2.java
@@ -0,0 +1,10 @@
+package Logic;
+
+public class main2 {
+
+	public static void main(String[] args) {
+		subspace subespacio = new subspace();  
+		subespacio.sortArray(subespacio.createList());
+	}
+
+}
