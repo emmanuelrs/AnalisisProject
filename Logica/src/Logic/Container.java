52a0693b17049f03052df039377ba3c4ebdffdd3
Emmanuel Rosales <emmanuelrs@gmail.com>
2015-05-30 11:21:40 -0600

    Clases Container, Packages

--- Logica/src/Logic/Container.java
+++ Logica/src/Logic/Container.java
@@ -12,8 +12,53 @@ public class Container {
 	/**
 	 * 
 	 */
+	
+	private double containerLength;
+	private double containerWidth;
+	private double containerHeight;
+	
+	public Container(double pLength, double pWidth, double pHeight) {
+		// TODO Auto-generated constructor stub
+		setContainerLength(pLength);
+		setContainerWidth(pWidth);
+		setContainerHeight(pHeight);	
+	}
+	
 	public Container() {
 		// TODO Auto-generated constructor stub
+		
+		// Si no se quiere poner un tamaÑo especifico se crea uno
+		// con medidas default
+		
+		setContainerLength(12);
+		setContainerWidth(3);
+		setContainerHeight(4);	
 	}
+	
+	public double getContainerLength() {
+		return containerLength;
+	}
+
+	public void setContainerLength(double containerLength) {
+		this.containerLength = containerLength;
+	}
+
+	public double getContainerWidth() {
+		return containerWidth;
+	}
+
+	public void setContainerWidth(double containerWidth) {
+		this.containerWidth = containerWidth;
+	}
+
+	public double getContainerHeight() {
+		return containerHeight;
+	}
+
+	public void setContainerHeight(double containerHeight) {
+		this.containerHeight = containerHeight;
+	}
+
+	
 
 }
--- Logica/src/Logic/Packages.java
+++ Logica/src/Logic/Packages.java
@@ -12,8 +12,67 @@ public class Packages {
 	/**
 	 * 
 	 */
-	public Packages() {
+	
+	private double packageLength;
+	private double packageWidth;
+	private double packageHeight;
+	private int diasEntrega;
+	private String nombre; // Descripción breve del paquete.
+	
+	
+
+	public Packages(double aPackageLength, double aPackageWidth, double aPackageHeight, int aDiasEntrega) {
 		// TODO Auto-generated constructor stub
+		
+		setPackageLength(aPackageLength);
+		setPackageWidth(aPackageWidth);
+		setPackageHeight(aPackageHeight);
+		setDiasEntrega(aDiasEntrega);
+		
 	}
+
+	public int getDiasEntrega() {
+		return diasEntrega;
+	}
+
+	public void setDiasEntrega(int diasEntrega) {
+		
+		// Si el valor de diasEntrega es igual a 0, el paquete no tiene 
+		// restricción de entrega
+		
+		this.diasEntrega = diasEntrega;
+	}
+
+	public double getPackageLength() {
+		return packageLength;
+	}
+
+	public void setPackageLength(double packageLength) {
+		this.packageLength = packageLength;
+	}
+
+	public double getPackageWidth() {
+		return packageWidth;
+	}
+
+	public void setPackageWidth(double packageWidth) {
+		this.packageWidth = packageWidth;
+	}
+
+	public double getPackageHeight() {
+		return packageHeight;
+	}
+
+	public void setPackageHeight(double packageHeight) {
+		this.packageHeight = packageHeight;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 
 }
