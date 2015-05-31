10aa2b38cb8201625f001c2bc9afbeb2341b0b70
Emmanuel Rosales <emmanuelrs@gmail.com>
2015-05-31 08:08:16 -0600

    Update

--- PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/InsertTruck.java
+++ PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/InsertTruck.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Trucks extends ActionBarActivity {
+public class InsertTruck extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trucks);
+        setContentView(R.layout.activity_insert_truck);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_trucks, menu);
+        getMenuInflater().inflate(R.menu.menu_insert_truck, menu);
         return true;
     }
 
