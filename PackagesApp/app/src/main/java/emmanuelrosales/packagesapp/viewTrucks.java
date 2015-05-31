5506988dde36c471f5b29c69dc17ff0915e88cca
Emmanuel Rosales <emmanuelrs@gmail.com>
2015-05-31 13:31:04 -0600

    Imprimir gridView

--- PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/viewTrucks.java
+++ PackagesApp/app/src/main/java/emmanuelrosales/packagesapp/viewTrucks.java
@@ -5,15 +5,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
 
 
 public class viewTrucks extends ActionBarActivity {
+
+
+    GridView grid;
+    static final String[] letters = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_trucks);
+        grid = (GridView) findViewById(R.id.gridViewTrucks);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, letters);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
